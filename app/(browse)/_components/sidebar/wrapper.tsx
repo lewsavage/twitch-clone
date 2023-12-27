3f948bc0d436c97a6b463b906949166fd1596132
Lew Savage <LS@savagesoundsinc.com>
2023-12-26 21:33:21 -0500

    initial commit

---
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useSidebar } from "@/use-sidebar";
+import { cn } from '@/lib/utils';
+import { useEffect, useState } from "react";
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended"
+import { useIsClient } from "usehooks-ts";
+
+interface WrapperProps {
+    children: React.ReactNode;
+};
+
+export const Wrapper = ({ children }: WrapperProps) => {
+    const { collapsed } = useSidebar((state) => state);
+    const isClient = useIsClient();
+
+
+    if (!isClient) {
+        return (
+            <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2d2e35] z-50">
+                <ToggleSkeleton />
+                <RecommendedSkeleton />
+            </aside>
+        )
+    }
+    return (
+        <aside
+            className={cn(
+                "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2d2e35] z-50",
+                collapsed && "w-[70px]")}
+        >
+            {children}
+        </aside>
+    )
+}
